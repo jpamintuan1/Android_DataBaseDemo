78e7e878b05c4cd0ba94ef728cb17cbc67f5337e
jpamintuan1 <jpamintuan1@gmail.com>
2018-05-30 15:45:22 -0700

    first

--- app/src/main/java/com/example/josep/databasedemo/MainActivity.java
+++ app/src/main/java/com/example/josep/databasedemo/MainActivity.java
@@ -18,41 +18,44 @@ public class MainActivity extends AppCompatActivity {
 
         try {
 
-            SQLiteDatabase eventsDB = this.openOrCreateDatabase("Events", MODE_PRIVATE, null);
+            SQLiteDatabase eventsDB = this.openOrCreateDatabase("Users", MODE_PRIVATE, null);
 
-            eventsDB.execSQL("CREATE TABLE IF NOT EXISTS events (event VARCHAR, year INT(4))");
 
-            eventsDB.execSQL("INSERT INTO events (event, year) VALUES ('End Of WW2', 1945)");
+            eventsDB.execSQL("CREATE TABLE IF NOT EXISTS newUsers (name VARCHAR, age INTEGER(3), id INTEGER PRIMARY KEY)");
 
-            eventsDB.execSQL("INSERT INTO events (event, year) VALUES ('Wham split up', 1986)");
+            eventsDB.execSQL("INSERT INTO newUsers (name, age) VALUES ('ASS', 21)");
 
-            Cursor c = eventsDB.rawQuery("SELECT * FROM events", null);
+            eventsDB.execSQL("INSERT INTO newUsers (name, age) VALUES ('FUCKYOU', 1)");
 
-            int eventIndex = c.getColumnIndex("event");
-            int yearIndex = c.getColumnIndex("year");
+            //eventsDB.execSQL("DELETE FROM newUsers WHERE id = 1");
+
+            Cursor c = eventsDB.rawQuery("SELECT * FROM newUsers", null);
+
+            int nameIndex = c.getColumnIndex("name");
+            int ageIndex = c.getColumnIndex("age");
+            int idIndex = c.getColumnIndex("id");
 
             c.moveToFirst();
 
             while (c != null) {
 
-                Log.i("Results - event", c.getString(eventIndex));
-                Log.i("Results - year", Integer.toString(c.getInt(yearIndex)));
+                Log.i("UserResults - name", c.getString(nameIndex));
+                Log.i("UserResults - age", Integer.toString(c.getInt(ageIndex)));
+                Log.i("UserResults - id", Integer.toString(c.getInt(idIndex)));
 
                 c.moveToNext();
             }
 
 
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
 
             e.printStackTrace();
 
         }
 
-
-
-
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
